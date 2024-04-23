6524ec13393dd9990ff3cb30228ab5f3be5a465f
page1597 <62283847+page1597@users.noreply.github.com>
2024-04-23 10:44:10 +0900

    Feat: 폼 스키마 수정

--- validators/auth.ts
+++ validators/auth.ts
@@ -1,19 +1,23 @@
 import { z } from "zod";
-const startWith010 = new RegExp(/^010/);
+const phoneRegex = /^010\d{8}$/;
 const passwordRegex =
-  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/;
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
 
 export const formSchema = z.object({
-  username: z.string().min(2, "이름은 2글자 이상이어야 합니다."),
+  username: z
+    .string()
+    .min(2, "이름은 2글자 이상이어야 합니다.")
+    .max(100, "이름은 100글자 이하이어야 합니다."),
   email: z.string().email("올바른 이메일을 입력해주세요."),
   phone: z
     .string()
     .length(11, "연락처는 11자리여야 합니다.")
-    .regex(startWith010, "010으로 시작하는 11자리 숫자를 입력해주세요."),
+    .regex(phoneRegex, "010으로 시작하는 11자리 숫자를 입력해주세요."),
   role: z.string().min(1, "역할을 선택해주세요."),
   password: z
     .string()
     .min(6, "비밀번호는 최소 6자리 이상이어야 합니다.")
+    .max(100, "비밀번호는 100자리 이하이어야 합니다.")
     .regex(
       passwordRegex,
       "비밀번호는 최소 6자리 이상, 영문, 숫자, 특수문자를 포함해야 합니다."
@@ -21,10 +25,9 @@ export const formSchema = z.object({
   confirmPassword: z
     .string()
     .min(6, "비밀번호는 최소 6자리 이상이어야 합니다.")
+    .max(100, "비밀번호는 100자리 이하이어야 합니다.")
     .regex(
       passwordRegex,
       "비밀번호는 최소 6자리 이상, 영문, 숫자, 특수문자를 포함해야 합니다."
     ),
-  //   password: z.string(),
-  //   confirmPassword: z.string(),
 });
