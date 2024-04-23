585164d9d06f864c4a1138e43417b5523c7dcdeb
page1597 <62283847+page1597@users.noreply.github.com>
2024-04-23 11:13:20 +0900

    Fix: Extra attributes from the server 에러 해결
    - 다크모드 구현 시 사용되는 ThemeProvider를 마운트 되었을때만 반환하도록 설정

--- components/themeProvider.tsx
+++ components/themeProvider.tsx
@@ -5,5 +5,15 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const [isMount, setMount] = React.useState(false);
+
+  React.useEffect(() => {
+    setMount(true);
+  }, []);
+
+  if (!isMount) {
+    return null;
+  }
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
